4a19a1cc7f88bd1062fea47844c9c5562cf2f22b
Kaya12345k <72180342+Kaya12345k@users.noreply.github.com>
2024-08-21 05:12:50 +0300

    new feature

--- staking_controller.go
+++ staking_controller.go
@@ -1,65 +1,81 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"strconv"
-	"github.com/joho/godotenv"
+    "encoding/json"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "github.com/joho/godotenv"
 )
 
 type Stake struct {
-	ID     int     `json:"id"`
-	UserID string  `json:"userId"`
-	Amount float64 `json:"amount"`
+    ID     int     `json:"id"`
+    UserID string  `json:"userId"`
+    Amount float64 `json:"amount"`
 }
 
 var stakes = []Stake{
-	{ID: 1, UserID: "123", Amount: 100.50},
+    {ID: 1, UserID: "123", Amount: 100.50},
 }
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
 }
 
 func main() {
-	http.HandleFunc("/createStake", createStakeHandler)
-	http.HandleFunc("/getStakes", getStakesHandler)
+    http.HandleFunc("/createStake", createStakeHandler)
+    http.HandleFunc("/getStakes", getStakesHandler)
+    http.HandleFunc("/getTotalStaked", getTotalStakedHandler) 
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil))
+    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil))
 }
 
 func createStakeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
-		return
-	}
+    if r.Method != "POST" {
+        http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+        return
+    }
 
-	var newStake Stake
-	err := json.NewDecoder(r.Body).Decode(&newStake)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+    var newStake Stake
+    err := json.NewDecoder(r.Body).Decode(&newStake)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
 
-	newStake.ID = len(stakes) + 1
-	stakes = append(stakes, newStake)
+    newStake.ID = len(stakes) + 1
+    stakes = append(stakes, newStake)
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(newStake)
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(newStake)
 }
 
 func getStakesHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, "Only GET method is allowed", http.StatusMethodNotAllowed)
-		return
-	}
+    if r.Method != "GET" {
+        http.Error(w, "Only GET method is allowed", http.StatusMethodNotAllowed)
+        return
+    }
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(stakes)
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(stakes)
+}
+
+func getTotalStakedHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method != "GET" {
+        http.Error(w, "Only GET method is allowed", http.StatusMethodNotAllowed)
+        return
+    }
+
+    totalStaked := 0.0
+    for _, stake := range stakes {
+        totalStaked += stake.Amount
+    }
+
+    response := map[string]float64{"totalStaked": totalStaked}
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(response)
 }
