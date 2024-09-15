03c8d2bd6b754ae312efcc35b5f828102fe57965
Kaya12345k <72180342+Kaya12345k@users.noreply.github.com>
2024-09-15 13:55:13 +0300

    improve readability

--- models.go
+++ models.go
@@ -1,52 +1,56 @@
 package main
 
 import (
-	"os"
+    "fmt"
+    "os"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
 )
 
 var (
-	dbHost     = os.Getenv("DB_HOST")
-	dbPort     = os.Getenv("DB_PORT")
-	dbUser     = os.Getenv("DB_USER")
-	dbPassword = os.Getenv("DB_PASSWORD")
-	dbName     = os.Getenv("DB_NAME")
+    dbHost     = os.Getenv("DB_HOST")
+    dbPort     = os.Getenv("DB_PORT")
+    dbUser     = os.Getenv("DB_USER")
+    dbPassword = os.Getenv("DB_PASSWORD")
+    dbName     = os.Getenv("DB_NAME")
 )
 
-var dbConnStr = "host=" + dbHost + " port=" + dbPort + " user=" + dbUser + " password=" + dbPassword + " dbname=" + dbName + " sslmode=disable"
+var dbConnStr = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+    dbHost, dbPort, dbUser, dbPassword, dbName)
 
 type User struct {
-	gorm.Model
-	Name     string
-	Email    string `gorm:"unique"`
-	Password string
+    gorm.Model
+    Name     string
+    Email    string `gorm:"unique"`
+    Password string
 }
 
 type Product struct {
-	gorm.Model
-	Name        string
-	Description string
-	Price       float64
+    gorm.Model
+    Name        string
+    Description string
+    Price       float64
 }
 
 func initDB() *gorm.DB {
-	db, err := gorm.Open(postgres.Open(dbConnStr), &gorm.Config{})
-	if err != nil {
-		panic("Failed to connect to database")
-	}
-	db.AutoMigrate(&User{}, &Product{})
+    db, err := gorm.Open(postgres.Open(dbConnStr), &gorm.Config{})
+    if err != nil {
+        panic(fmt.Sprintf("Failed to connect to database: %v", err))
+    }
+    if err := db.AutoMigrate(&User{}, &Product{}); err != nil {
+        panic(fmt.Sprintf("Failed to automatically migrate database schema: %v", err))
+    }
 
-	return db
+    return db
 }
 
 func main() {
-	db := initDB()
+    db := initDB()
 
-	newUser := User{Name: "John Doe", Email: "john.doe@example.com", Password: "supersecurepassword"}
-	result := db.Create(&newUser) 
-	if result.Error != nil {
-		panic(result.Error)
-	}
+    newUser := User{Name: "John Doe", Email: "john.doe@example.com", Password: "supersecurepassword"}
+    result := db.Create(&newUser)
+    if result.Error != nil {
+        panic(fmt.Sprintf("Failed to create new user: %v", result.Error))
+    }
 }
