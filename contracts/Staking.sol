d84308ea9c303c0038b9cb49e920f7e555aa6807
Kaya12345k <72180342+Kaya12345k@users.noreply.github.com>
2024-12-23 20:08:05 +0200

    update error catching

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -2,10 +2,13 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract StakingContract is Ownable {
+contract StakingContract is Ownable, ReentrancyGuard {
     using SafeMath for uint256;
+    using SafeERC20 for IERC20;
 
     IERC20 public stakingToken; 
 
@@ -17,18 +20,19 @@ contract StakingContract is Ownable {
     mapping(address => Stake) public stakes;
 
     event Staked(address indexed user, uint256 amount, uint256 timestamp);
+    event Withdrawn(address indexed user, uint256 amount);
 
     constructor(IERC20 _stakingToken) {
         stakingToken = _stakingToken;
     }
 
-    function stakeTokens(uint256 _amount) external {
+    function stakeTokens(uint256 _amount) external nonReentrant {
         require(_amount > 0, "Cannot stake 0 tokens");
-
-        stakingToken.transferFrom(msg.sender, address(this), _amount);
 
         stakes[msg.sender].amount = stakes[msg.sender].amount.add(_amount);
         stakes[msg.sender].since = block.timestamp;
+
+        stakingToken.safeTransferFrom(msg.sender, address(this), _amount);
 
         emit Staked(msg.sender, _amount, block.timestamp);
     }
@@ -37,11 +41,14 @@ contract StakingContract is Ownable {
         return stakes[_staker].amount;
     }
 
-    function withdrawTokens(uint256 _amount) external {
+    function withdrawTokens(uint256 _amount) external nonReentrant {
         require(_amount > 0, "Cannot withdraw 0 tokens");
         require(stakes[msg.sender].amount >= _amount, "Withdrawal request exceeds staked amount");
 
         stakes[msg.sender].amount = stakes[msg.sender].amount.sub(_amount);
-        stakingToken.transfer(msg.sender, _amount);
+
+        stakingToken.safeTransfer(msg.sender, _amount);
+
+        emit Withdrawn(msg.sender, _amount);
     }
 }
